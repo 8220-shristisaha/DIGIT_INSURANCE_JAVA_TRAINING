d9ac0e51c796fa13e5049f1d75ff9f848c8aa8db
Shri <Shristi.Saha@godigit.com>
2024-07-25 03:36:21 +0530

    Default Value Of Primitive Type

---
+++ Day-1/DefaultValueOfPrimitiveType.java
@@ -0,0 +1,26 @@
+
+public class DefaultValueOfPrimitiveType {
+    int intVariable;
+    float floatVariable;
+    double doubleVariable;
+    boolean booleanVariable;
+    char charVariable;
+    long longVariable;
+    byte byteVariable;
+    short shortVariable;
+
+
+    public static void main(String[] args) {
+
+        DefaultValueOfPrimitiveType main = new DefaultValueOfPrimitiveType();
+        System.out.println("Default Value of int : "+main.intVariable);
+        System.out.println("Default Value of float : "+main.floatVariable);
+        System.out.println("Default Value of double : "+main.doubleVariable);
+        System.out.println("Default Value of boolean  : "+main.booleanVariable);
+        System.out.println("Default Value of char  : "+main.charVariable);
+        System.out.println("Default Value of long  : "+main.longVariable);
+        System.out.println("Default Value of byte   : "+main.byteVariable);
+        System.out.println("Default Value of short   : "+main.shortVariable);
+
+    }
+}
