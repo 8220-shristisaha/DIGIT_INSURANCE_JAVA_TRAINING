b27c740b07bfa047d71225299c41a9d2609c2b7b
Shri <Shristi.Saha@godigit.com>
2024-07-25 03:49:01 +0530

    Strings are Equal or not

---
+++ Day-1/StringEquals.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class StringEquals{
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String value1;
+        String value2;
+
+        System.out.println("Enter value1 : ");
+        value1= scanner.next();
+
+        System.out.println("Enter value1 : ");
+        value2= scanner.next();
+
+
+        if(value1.equals(value2)){
+            System.out.println("String value1 & value2 are equals");
+        }
+        else{
+            System.out.println("String value1 & value2 are not equals");
+
+        }
+    }
+}
